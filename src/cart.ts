c9ea15dbfb70ca3c240b24f9b06dcba42ecf2237
Vinicius Silva <viniciusvilanova7@gmail.com>
2025-11-24 13:24:47 -0300

    Lista dinamica

--- src/cart.ts
+++ src/cart.ts
@@ -1,5 +1,6 @@
 import { setupButtonStates } from './buttonStates';
 import type { Lista } from './types/Lista';
+import type { Product } from './types/Product';
 
 export const cart = () => {
   const btnsAddCart = document.querySelectorAll('.btn-add-to-cart');
@@ -83,24 +84,28 @@ export const cart = () => {
     }
   });
 
-  const atualizaCart = () => {
+  const atualizaCart = async () => {
     const listaDeCompras: Lista[] = JSON.parse(
       localStorage.getItem('listaDeCompras') || '[]',
     );
+
+    const divCartItems = document.querySelector(
+      '.cart-items',
+    ) as HTMLDivElement;
+
+    // Zera HTML sempre antes de montar
+    divCartItems.innerHTML = '';
 
     const totalItems = listaDeCompras.reduce(
       (acc, item) => acc + item.quantidade,
       0,
     );
 
+    // Atualiza contador
     const cartCount = document.querySelector(
       '.qtd-produtos',
     ) as HTMLSpanElement;
     cartCount.textContent = `(${totalItems})`;
-
-    const divCartItems = document.querySelector(
-      '.cart-items',
-    ) as HTMLDivElement;
 
     // Carrinho vazio
     if (totalItems === 0) {
@@ -108,30 +113,65 @@ export const cart = () => {
       <div class="flex flex-col justify-center items-center gap-y-3">
         <img src="/public/images/illustration-empty-cart.svg" class="w-1/3">
         <p class="text-base text-rose-300 font-bold">Your added items will appear here</p>
-      </div>
-    `;
+      </div>`;
       return;
     }
 
-    // Carrinho com itens (dinâmico futuramente)
-    divCartItems.innerHTML = `
-    <div class="flex flex-col gap-y-3">
-      <ul class="cart-list"></ul>
+    //Aqui eu deveria fazer um Try Catch, mas como o fetch é rápido e simples, deixei assim
+    const response = await fetch('/data/data.json');
+    const products: Product[] = await response.json();
 
-      <div class="flex flex-row justify-between items-center mb-3">
-        <p class="text-base text-rose-900 font-normal">Order Total</p>
-        <h2 class="text-3xl font-bold text-rose-900 total-price">$0.00</h2>
+    const ul = document.createElement('ul');
+    divCartItems.appendChild(ul);
+
+    let totalOrder = 0;
+
+    listaDeCompras.forEach((item) => {
+      const produto = products.find((p) => String(p.id) === item.id);
+      if (!produto) return;
+
+      const totalItem = produto.price * item.quantidade;
+      totalOrder += totalItem;
+
+      const li = document.createElement('li');
+      li.classList.add(
+        'flex',
+        'justify-between',
+        'items-center',
+        'border-b',
+        'border-rose-100',
+        'mb-3',
+      );
+
+      li.innerHTML = `
+      <div class="flex flex-col gap-y-2">
+        <p class="font-semibold text-rose-900 text-base">${produto.name}</p>
+
+        <div class="pb-3">
+          <span class="text-red-50 font-semibold pr-3">${item.quantidade}x</span>
+          <span class="text-rose-300 font-semibold pr-1">@ $${produto.price.toFixed(2)}</span>
+          <span class="font-semibold text-rose-500">$${totalItem.toFixed(2)}</span>
+        </div>
       </div>
 
-      <div class="flex flex-row justify-center items-center gap-x-2 bg-rose-100 p-4 rounded-xl mb-3">
-        <img src="src/assets/img/icon-carbon-neutral.svg" alt="" />
-        <p>This is a <span class="font-semibold text-rose-900">carbon-neutral</span> delivery</p>
-      </div>
+      <button type="button" class="btn-remove-item" data-id="${item.id}">X</button>
+    `;
 
-      <button class="bg-red-50 text-rose-50 p-4 rounded-4xl cursor-pointer">
-        Confirm Order
-      </button>
+      ul.appendChild(li);
+    });
+
+    divCartItems.innerHTML += `
+    <div class="flex flex-row justify-between items-center mb-3">
+      <p class="text-base text-rose-900 font-normal">Order Total</p>
+      <h2 class="text-3xl font-bold text-rose-900">$${totalOrder.toFixed(2)}</h2>
     </div>
+
+    <div class="flex flex-row justify-center items-center gap-x-2 bg-rose-100 p-4 rounded-xl mb-3">
+      <img src="public/images/icon-carbon-neutral.svg">
+      <p>This is a <span class="font-semibold text-rose-900">carbon-neutral</span> delivery</p>
+    </div>
+
+    <button type="submit" class="bg-red-50 text-rose-50 p-4 rounded-4xl cursor-pointer">Confirm Order</button>
   `;
   };
 
